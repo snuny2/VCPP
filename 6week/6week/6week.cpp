630a4e00bc3346f2f8a825aca03a97d75a45b6c5
snuny2 <efzxcsh20@gmail.com>
2023-10-14 18:20:47 +0900

    깜박임은 어떻게 해야하는가...

---
+++ 6week/6week/6week.cpp
@@ -0,0 +1,215 @@
+﻿//#define DEBUG
+#ifdef UNICODE
+#ifdef DEBUG
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+#else
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:WINDOWS")
+#endif // DEBUG
+#else
+#ifdef DEBUG
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:WINDOWS")
+#endif // DEBUG
+#endif
+
+#include <windows.h>
+#include <stdbool.h>
+
+// 타이머 ID 정의
+#define TIMER_ID 1
+
+POINT startPoint = { 0 };
+POINT endPoint = { 0 };
+int isKeyPressed = 0;
+
+RECT rect_user = { 5, 5, 10, 10 }; // 왼쪽 상단 좌표 (50, 50)에서 오른쪽 하단 좌표 (150, 150)까지의 사각형
+RECT rect_target = { 50, 50, 150, 150 }; // 왼쪽 상단 좌표 (50, 50)에서 오른쪽 하단 좌표 (150, 150)까지의 사각형
+
+// 필요한 전역 변수 추가
+bool isCrashed = false;
+int dx = 0; // x 방향으로의 이동 거리
+int dy = 0; // y 방향으로의 이동 거리
+int speed = 1; // 이동속도
+
+// 윈도우의 이벤트를 처리하는 콜백(Callback) 함수.
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc = GetDC(hwnd);
+
+	HBRUSH hBrush_user = CreateSolidBrush(RGB(0, 0, 255));
+	HBRUSH hBrush_target = CreateSolidBrush(RGB(255, 0, 255));
+	HBRUSH hBrush_eraser = CreateSolidBrush(RGB(255, 255, 255));
+	const wchar_t* text = L"Crash!!!";
+
+	switch (uMsg)
+	{
+		// WM_KEYDOWN 메시지 핸들러 수정
+	case WM_KEYDOWN:
+		isKeyPressed = 1;
+		if (wParam == VK_RIGHT)
+		{
+			dx = speed;
+		}
+		else if (wParam == VK_LEFT)
+		{
+			dx = -speed;
+		}
+		else if (wParam == VK_UP)
+		{
+			dy = -speed;
+		}
+		else if (wParam == VK_DOWN)
+		{
+			dy = speed;
+		}
+		SetTimer(hwnd, 1, 500, NULL); // 타이머 시작, 10ms 간격으로 타이머 메시지 발생
+		break;
+		// WM_KEYUP 메시지 핸들러 추가
+	case WM_KEYUP:
+		isKeyPressed = 0;
+		dx = 0;
+		dy = 0;
+		KillTimer(hwnd, 1); // 타이머 종료
+		break;
+
+	case WM_PAINT:
+	{
+		if (isKeyPressed)
+		{
+			FillRect(hdc, &rect_user, hBrush_user);
+			FillRect(hdc, &rect_target, hBrush_target);
+			rect_user.left += dx;
+			rect_user.right += dx;
+			rect_user.top += dy;
+			rect_user.bottom += dy;
+			InvalidateRect(hwnd, NULL, TRUE);
+
+			// 파란 박스와 분홍 박스 간의 충돌을 감지
+			if (rect_user.left >= rect_target.left && rect_user.top >= rect_target.top &&
+				rect_user.right <= rect_target.right && rect_user.bottom <= rect_target.bottom)
+			{
+				isCrashed = true;
+				TextOut(hdc, 10, 10, L"들어갔다!!", lstrlen(L"들어갔다!!"));
+			}
+			else
+			{
+				isCrashed = false;
+			}
+		}
+		else
+		{
+			if (!isCrashed)
+			{
+				TextOut(hdc, 10, 10, L"Crash!!!", lstrlen(L"Crash!!!"));
+			}
+			FillRect(hdc, &rect_user, hBrush_eraser);
+			FillRect(hdc, &rect_target, hBrush_target);
+		}
+	}
+	break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	DeleteObject(hBrush_user);
+	DeleteObject(hBrush_target);
+	DeleteObject(hBrush_eraser);
+	ReleaseDC(hwnd, hdc);
+
+	return S_OK;
+}
+#ifdef UNICODE
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+#else
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR pCmdLine, _In_ int nCmdShow)
+#endif
+{
+	/* 윈도우 클래스 선언.*/
+	WNDCLASS wc;
+	ZeroMemory(&wc, sizeof(wc));	// 모두 0으로 초기화.
+
+	// 윈도우 클래스 값 설정
+	wc.hInstance = hInstance;
+	wc.lpszClassName = TEXT("Computer Software");
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_CROSS);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpfnWndProc = WindowProc;
+
+	// 윈도우 클래스 등록.
+	if (RegisterClass(&wc) == 0)
+	{
+		MessageBox(NULL, L"RegisterClass failed!", L"Error", MB_ICONERROR);
+		exit(-1);	//예외
+	}
+
+	// Window viewport 영역 조정
+	RECT rect = { 150, 100, 800, 600 };
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, 0);
+	int width = rect.right - rect.left;
+	int height = rect.bottom - rect.top;
+
+	// 윈도우 생성
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		TEXT("202307075_김선호"),
+		WS_OVERLAPPEDWINDOW,
+		0, 0,
+		width, height,
+		NULL, NULL,
+		hInstance,
+		NULL
+	);
+
+	// 오류 검사.
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, L"CreateWindow failed!", L"Error", MB_ICONERROR);
+		exit(-1);
+	}
+
+	// 창 보이기.
+	ShowWindow(hwnd, SW_SHOW); // 창 띄우고
+	UpdateWindow(hwnd); // 업데이트해야 보임. 한 쌍으로 쓴다고 보면 됨.
+
+
+
+	// 메시지 처리 루프.
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+
+	// 메시지 처리.
+	while (msg.message != WM_QUIT)
+	{
+		if (GetMessage(&msg, NULL, 0, 0))
+		{
+			// 메시지 해석해줘.
+			TranslateMessage(&msg);
+			// 메시지를 처리해야할 곳에 전달해줘.
+			DispatchMessage(&msg);
+		}
+		//if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		//{
+		//	// 메시지 해석해줘.
+		//	TranslateMessage(&msg);
+		//	// 메시지를 처리해야할 곳에 전달해줘.
+		//	DispatchMessage(&msg);
+		//} // PM_REMOVE의 자리는 이 메세지를 쓰고 어떡할거냐.의 의미인데 지운다는 것임.
+		//else{}
+	}
+
+	UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+	//종료 메시지 보내기
+	return (int)msg.wParam;
+
+}
